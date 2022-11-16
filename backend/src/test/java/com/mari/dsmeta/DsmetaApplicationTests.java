8f7422c04326631a790e73f92e0d485e28f7f863
MariseteLuckner <mari_luckner@hotmail.com>
2022-11-15 22:56:07 -0300

    Projeto criado

---
+++ backend/src/test/java/com/mari/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mari.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
